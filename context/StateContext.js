81af8e0647e4dae3dec4e71029d5f5b6147321ed
Punit Kumar Saini <64743687+punit-saini@users.noreply.github.com>
2023-02-23 15:35:06 +0530

    redeploying on vercel

--- context/StateContext.js
+++ context/StateContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
 
 const Context = createContext();
-
+ 
 export const StateContext = ({children}) => {
       
   
