87470c4de7ecfd9f083a6aaa6e02ca3133f61ac1
chhattisgarhchess <developer.cgsca@gmail.comm>
2023-07-13 15:53:26 +0530

    change order-history page

--- pages/my-profile/my-orders.js
+++ pages/my-profile/my-orders.js
@@ -7,93 +7,81 @@ import Users from "@/models/User";
 import mongoose from "mongoose";
 import { client } from "@/lib/client";
 
-export default function(props) {
+export default function OrderHistoryPage(props) {
   const { orders } = props;
   // const session = useSession();
 
- 
-    return (
-      <div className="container mx-auto my-10 flex flex-wrap w-11/12">
-        <h1 className="w-full text-3xl font-bold mb-6">Order History</h1>
-        {orders.length>=1 ? orders.map((order) => {
-          return (
-            <div
-              key={order._id}
-              className="w-full md:w-1/2 border-2 border-gray-200 shadow-md rounded-lg mb-8 p-4"
-            >
-              <div className="flex justify-between">
-                <div>
-                  <h2 className="font-bold text-lg">
-                    Order Date: {order.orderDate}
-                  </h2>
-                  <p className="text-gray-500 text-sm mt-2 md:mt-0">
-                    Order Status: {order.orderStatus}
-                  </p>
-                </div>
-                <div className="text-right">
-                  <p className="text-gray-500 text-sm">
-                    Order Amount: ₹{order.totalPrice}
-                  </p>
-                </div>
-              </div>
-              <hr className="my-4" />
-              <div>
-                <h3 className="font-bold text-lg">Products</h3>
-                <div className="mt-2">
-                  {order.orderItems.map((product) => {
-                    return (
-                      <div
-                        key={product._key}
-                        className="flex items-center justify-between mb-2"
-                      >
-                        <Link href={`/product/${product.url}`} className="font-semibold text-gray-700 hover:text-gray-800">
-                            {product.shortName}
-                          
-                        </Link>
-                        <span className="text-gray-600 text-sm">
-                          {product.quantity} x ₹{product.price}
-                        </span>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-              <hr className="my-4" />
-              <div className="flex justify-between items-center">
-                <div>
-                  <h3 className="font-bold text-lg">
-                    Order For: {order.orderName}
-                  </h3>
-                  <p className="text-gray-500">
-                    Delivery Address: {order.address.street}
-                  </p>
-                </div>
-                <div>
-                  <p className="text-gray-500 text-sm">
-                    Order Status: {order.orderStatus}
-                  </p>
-                </div>
-              </div>
+  return (
+    <div className="container mx-auto my-10 w-11/12">
+      <h1 className="text-3xl font-bold mb-6">Order History</h1>
+      {orders.length >= 1 ? (
+        orders.map((order) => (
+          <div
+            key={order._id}
+            className="border-2 border-gray-200 shadow-md rounded-lg mb-8 p-4"
+          >
+            <div>
+              <h2 className="font-bold text-lg mb-2">
+                Order Date: {order.orderDate}
+              </h2>
+              <p className="text-gray-500 text-sm">
+                Order Status: {order.orderStatus}
+              </p>
+              <p className="text-gray-500 text-sm">
+                Order Amount: ₹{order.totalPrice}
+              </p>
             </div>
-          );
-        }): <p>No Orders Yet</p>}
-      </div>
-    );
-  
+            <hr className="my-4" />
+            <div>
+              <h3 className="font-bold text-lg mb-2">Products</h3>
+              {order.orderItems.map((product) => (
+                <div
+                  key={product._key}
+                  className="flex items-center justify-between mb-2"
+                >
+                  <Link
+                    href={`/product/${product.url}`}
+                    className="font-semibold text-gray-700 hover:text-gray-800"
+                  >
+                    {product.shortName}
+                  </Link>
+                  <span className="text-gray-600 text-sm">
+                    {product.quantity} x ₹{product.price}
+                  </span>
+                </div>
+              ))}
+            </div>
+            <hr className="my-4" />
+            <div>
+              <h3 className="font-bold text-lg mb-2">
+                Order For: {order.orderName}
+              </h3>
+              <p className="text-gray-500">
+                Delivery Address: {order.address.street}
+              </p>
+              <p className="text-gray-500 text-sm">
+                Order Status: {order.orderStatus}
+              </p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No Orders Yet</p>
+      )}
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
-  if (!session){
+  if (!session) {
     return {
-        redirect : {
-          destination : '/sign-in',
-          permanent : false,
-        }
-      }
-  }
-  else {
-
+      redirect: {
+        destination: "/sign-in",
+        permanent: false,
+      },
+    };
+  } else {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -109,4 +97,4 @@ export async function getServerSideProps(context) {
       },
     };
   }
- }
+}
